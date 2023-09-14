c926b0ec1fd7e959e913b600a7c5dc4d460a0adb
tmoipolai <tmoipolai@csir.co.za>
2023-09-15 00:40:55 +0200

    Updates

---
+++ precache-manifest.ee4959de85e4200082cc560d1559231f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadae24d3c29c3537a1a0c6c9549ebb1",
+    "url": "/tmz-portfolio/index.html"
+  },
+  {
+    "revision": "9222e39b0c3af7a50ebd",
+    "url": "/tmz-portfolio/static/js/2.def8b33d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tmz-portfolio/static/js/2.def8b33d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e5109ab6a02f5b3f19",
+    "url": "/tmz-portfolio/static/js/main.e661c0e2.chunk.js"
+  },
+  {
+    "revision": "124502c9746815ca5405",
+    "url": "/tmz-portfolio/static/js/runtime-main.3266aa73.js"
+  },
+  {
+    "revision": "3dbc4fe5a44cd2134659b257474df5f2",
+    "url": "/tmz-portfolio/static/media/avatar.3dbc4fe5.jpg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/tmz-portfolio/static/media/bars.b3f49afd.svg"
+  },
+  {
+    "revision": "11a706e8bf04f97366f1c7d6f1dcfd3f",
+    "url": "/tmz-portfolio/static/media/facebook.11a706e8.svg"
+  },
+  {
+    "revision": "6ed3db481fc0668d4efbe2e1e887b8bd",
+    "url": "/tmz-portfolio/static/media/github.6ed3db48.svg"
+  },
+  {
+    "revision": "201afcb070acc5707cf61ffba66865d9",
+    "url": "/tmz-portfolio/static/media/instagram.201afcb0.svg"
+  },
+  {
+    "revision": "083538a628e5ba64621ed9d3547077e2",
+    "url": "/tmz-portfolio/static/media/linkedin.083538a6.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/tmz-portfolio/static/media/times.2c5552a1.svg"
+  }
+]);
